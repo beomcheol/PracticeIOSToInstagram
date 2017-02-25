c0e33d12d926d34b0cf1d3ed7376dfac17c3e152
Beomcheol Shin <beomcheol.shin@rgpkorea.co.kr>
2017-02-25 20:12:48 +0900

    implement auth view controller

---
+++ PracticeIOSToInstagram/AuthViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AuthViewController.swift
+//  PracticeIOSToInstagram
+//
+//  Created by Masher Shin on 25/02/2017.
+//  Copyright © 2017 Masher Shin. All rights reserved.
+//
+
+import UIKit
+import InstagramKit
+
+class AuthViewController: UIViewController {
+
+    @IBOutlet var webView: UIWebView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.loadRequest(URLRequest(url: InstagramEngine.shared().authorizationURL(for: .publicContent)))
+    }
+}
+
+// MARK: - UIWebViewDelegate
+extension AuthViewController: UIWebViewDelegate {
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        activityIndicator.stopAnimating()
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if let url = request.url {
+            do {
+                try InstagramEngine.shared().receivedValidAccessToken(from: url)
+//                modalTransitionStyle = .coverVertical
+                dismiss(animated: true, completion: nil)
+                return false
+            } catch {
+                print(error)
+            }
+        }
+        
+        return true
+    }
+}
